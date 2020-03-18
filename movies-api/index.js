f0b8f0a065ae87c4d0c03351daa30be7a6ec4510
Diego Camino <diego.camino.r@gmail.com>
2020-03-18 01:13:26 -0500

    Implementación de Boom, manejo de errores

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,17 +6,25 @@ const moviesApi = require('./routes/movies.js');
 
 const {
   logErrors,
+  wrapErrors,
   errorHandler
 } = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler.js');
 
 // body parser
 app.use(express.json()); // este middleware permite a nuestras rutas interpretar los datos en formato json que le enviamos
 
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
+// Error middleware
 // los middleware son una pieza de software que está entre 2, se le conoce como software glue, es decir ayuda a conectar otras piezas de software
 // los middleware de error siempre tienen que ir al final de nuestras rutas, recordemos que ellos también son rutas
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
--- movies-api/utils/middleware/errorHandlers.js
+++ movies-api/utils/middleware/errorHandlers.js
@@ -1,8 +1,9 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 function withErrorStack(error, stack) {
   if (config.dev) {
-    return { error, stack }; // el stack es toda la información relacionada al error
+    return { ...error, stack }; // el stack es toda la información relacionada al error
   }
   return error;
 }
@@ -12,12 +13,27 @@ function logErrors(err, req, res, next) {
   next(err);
 }
 
+// esta función se crea porque es posible que en algún punto del código
+// el error que llegue no sea de tipo boom y queremos que a partir de ahí
+// todos los errores tengan la estructura de boom
+function wrapErrors(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err)); // manera genérica de marcar el error
+  }
+  next(err);
+}
+
 function errorHandler(err, req, res, next) { // eslint-disable-line
-  res.status(err.status || 500);
-  res.json(withErrorStack(err.message, err.stack));
+  const {
+    output: { statusCode, payload }
+  } = err;
+
+  res.status(statusCode);
+  res.json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
   logErrors,
+  wrapErrors,
   errorHandler
 };
