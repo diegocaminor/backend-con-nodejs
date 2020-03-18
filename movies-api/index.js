0ffdf97e153562f568f721844cdfbe181cf9b7f6
Diego Camino <diego.camino.r@gmail.com>
2020-03-18 00:36:07 -0500

    Manejo de errores usando un middleware en express

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
+const {
+  logErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
+
+// body parser
 app.use(express.json()); // este middleware permite a nuestras rutas interpretar los datos en formato json que le enviamos
 
 moviesApi(app);
+
+// los middleware son una pieza de software que está entre 2, se le conoce como software glue, es decir ayuda a conectar otras piezas de software
+// los middleware de error siempre tienen que ir al final de nuestras rutas, recordemos que ellos también son rutas
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http//localhost:${config.port}`);
