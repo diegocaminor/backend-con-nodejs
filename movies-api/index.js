a4ca4ec3a6b68b90cf5499213a1e88c412963392
Diego Camino <diego.camino.r@gmail.com>
2020-04-05 21:27:06 -0500

    Corrección script dev

--- movies-api/index.js
+++ movies-api/index.js
@@ -36,5 +36,5 @@ app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
-  console.log(`Listening http//localhost:${config.port}`);
+  console.log(`Listening http://localhost:${config.port}`);
 });
